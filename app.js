ef7d5644d91dbebfe2b89a07d76397f42e3dd535
Angelo Chaknis <angelopalmtree@gmail.com>
2022-12-21 11:41:29 -0700

    bug: wrong file was inputted

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", async (req, res) => {
   // Create client instance for auth
   const client = await auth.getClient();
 
-  // Instance of Google Sheets API
+  // Instance of Google Sheets API n
 });
 
 app.listen(3000, (req, res) => {
